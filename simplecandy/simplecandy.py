f708111add1c2cd9425d42b64fd83a3a40ad592e
Mike Dorman <mike.dorman@sendgrid.com>
2017-12-22 21:08:23 -0700

    Better implementation

--- simplecandy/simplecandy.py
+++ simplecandy/simplecandy.py
@@ -6,18 +6,22 @@ import opc
 class SimpleCandy(object):
 
     def __init__(self, server_id_port, numpixels=50):
-        self.pixels = [(0, 0, 0)] * numpixels
         self.client = opc.Client(server_id_port)
+        self.numpixels = numpixels
+        self.lightOff()
 
-    def off(id=None):
-        if id:
-            self.pixels = [(0, 0, 0)] * numpixels
+    def lightOff(self, id=None):
+        self.lightOn(0, 0, 0, id)
+
+    def lightOn(self, r, g, b, id=None):
+        if id is not None:
+            self.pixels[id] = (0xff * g/100, 0xff * r/100, 0xff * b/100)
         else:
-            self.pixels[id] = (0, 0, 0)
+            self.pixels = [(0xff * g/100, 0xff * r/100, 0xff * b/100)] * self.numpixels
         self.__paint()
 
-    def lightOn(self, id, r, g, b):
-        self.pixels[id] = (0xff * r, 0xff * g, 0xff * b)
+    def pixels(self, pixels):
+        self.pixels = pixels
         self.__paint()
 
     def __paint(self):
